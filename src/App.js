2b215af4aaa2fb12207f04709d07cb2ded589cc7
Angela Sun <angelasun1995@gmail.com>
2022-05-26 23:10:10 -0400

    Add firebase

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+  apiKey: 'AIzaSyDy9hiNU6LsT1aSiavUnJ1S_L-OZB4-5JY',
+  authDomain: 'imgur-3d855.firebaseapp.com',
+  projectId: 'imgur-3d855',
+  storageBucket: 'imgur-3d855.appspot.com',
+  messagingSenderId: '906974734656',
+  appId: '1:906974734656:web:49df2c0734fd79218872a5',
+};
+
+initializeApp(firebaseConfig);
+
 function App() {
   return <>Hello from App!</>;
 }
