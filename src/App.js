28ba55c6329512c7a80bce8ec518f782fe358038
Angela Sun <angelasun1995@gmail.com>
2022-05-26 23:58:25 -0400

    Add sign-in and sign-out functions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,69 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, collection, addDoc } from 'firebase/firestore';
+import {
+  getAuth,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+import { useState } from 'react';
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyDy9hiNU6LsT1aSiavUnJ1S_L-OZB4-5JY',
-  authDomain: 'imgur-3d855.firebaseapp.com',
-  projectId: 'imgur-3d855',
-  storageBucket: 'imgur-3d855.appspot.com',
-  messagingSenderId: '906974734656',
-  appId: '1:906974734656:web:49df2c0734fd79218872a5',
+  apiKey: 'AIzaSyC9IYgw2O6Uyj_B_beQCloRf8NgZmc0hss',
+  authDomain: 'imgur-75071.firebaseapp.com',
+  projectId: 'imgur-75071',
+  storageBucket: 'imgur-75071.appspot.com',
+  messagingSenderId: '985443297553',
+  appId: '1:985443297553:web:17c350e2c15f014cb984b0',
 };
 
 initializeApp(firebaseConfig);
 
+const createPost = async () => {
+  const db = getFirestore();
+  const docRef = await addDoc(collection(db, 'posts'), {
+    title: 'Testing Title',
+    description: 'Testing Description',
+  });
+  console.log('Written document with ID', docRef.id);
+};
+
+createPost();
+
 function App() {
-  return <>Hello from App!</>;
+  const [isUserSignedIn, setIsUserSignedIn] = useState(false);
+  const signIn = async () => {
+    var provider = new GoogleAuthProvider();
+    await signInWithPopup(getAuth(), provider);
+  };
+
+  const signOutUser = () => {
+    signOut(getAuth());
+  };
+
+  const authStateObserver = (user) => {
+    setIsUserSignedIn(user ? true : false);
+  };
+  const initFirebaseAuth = () => {
+    onAuthStateChanged(getAuth(), authStateObserver);
+  };
+  initFirebaseAuth();
+
+  return (
+    <>
+      <div>Create post</div>
+      {isUserSignedIn ? (
+        <div className="sign-out" onClick={signOutUser}>
+          Sign Out
+        </div>
+      ) : (
+        <div className="sign-in" onClick={signIn}>
+          Sign In
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
