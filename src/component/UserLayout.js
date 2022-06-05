2d88f3208ed4f0d88e8e8c74a164e88e1b5cb67c
Angela Sun <angelasun1995@gmail.com>
2022-06-05 19:44:09 -0400

    fetch user data at user profile page

--- src/component/UserLayout.js
+++ src/component/UserLayout.js
@@ -1,12 +1,45 @@
 import { useParams, Outlet } from 'react-router-dom';
-
+import {
+  query,
+  collection,
+  getFirestore,
+  where,
+  getDocs,
+} from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 const UserLayout = () => {
   const { name } = useParams();
-
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const fetchUserData = async (username) => {
+      const q = query(
+        collection(getFirestore(), 'users'),
+        where('name', '==', username)
+      );
+      const querySnapshot = await getDocs(q);
+      let data;
+      querySnapshot.forEach((doc) => {
+        data = doc.data();
+      });
+      return data;
+    };
+    fetchUserData(name).then((data) => {
+      setUser(data);
+    });
+  }, [name]);
   return (
     <>
-      Hello {name}! Welcome to user page
-      <Outlet context={name} />
+      {user && (
+        <>
+          <div className="profile-header">
+            <div className="profile-container">
+              <img src={user.profilePic} alt={`${user.name} profile`} />
+              <div className="profile-name">{user.name}</div>
+            </div>
+          </div>
+          <Outlet context={name} />
+        </>
+      )}
     </>
   );
 };
